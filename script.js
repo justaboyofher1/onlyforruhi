9b2681144864801a02228fc57f6b49db645e94ae
justaboyofher1 <onlyforruhi@gmail.com>
2025-07-22 10:10:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// External JavaScript placeholder
+const btn = document.getElementById("music-btn");
+const music = document.getElementById("ruhiMusic");
+let isPlaying = false;
+
+btn.addEventListener("click", () => {
+  if (!isPlaying) {
+    music.play();
+    btn.textContent = "⏸️ Pause Song";
+    isPlaying = true;
+  } else {
+    music.pause();
+    btn.textContent = "▶️ Play Song";
+    isPlaying = false;
+  }
+});
